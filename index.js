af38a414ddf8307fac0002c6d80e6d730c75ca89
Itai <itai137@gmail.com>
2024-09-05 11:25:39 +0300

    added a searching option

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -14,15 +15,30 @@ app.get("/", async (req , res) => {
     try {
     const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php") 
     const result =  response.data.drinks[0];
-    console.log(`Drink name: ${result.strDrink}\nImg source: ${result.strDrinkThumb}`);
-    
+    //console.log(`Drink name: ${result.strDrink}\nImg source: ${result.strDrinkThumb}`);
+    console.log(`req.body is : ${req.body}`);
     res.render("index.ejs",{data: result});
     } catch (error){
         console.error("Failed to make request:", error.message);
     res.render("index.ejs", {error: error.message,})
     };
     });
+
+
+app.post("/search-cocktail", async (req, res) => {
     
+    try {
+        //console.log(`req.body is : ${req.body[0]}`);
+        const searchId = req.body.search;
+        console.log("search is :"+searchId);
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchId}`);
+        const result =  response.data.drinks[0];
+        
+        res.render("index.ejs",{data: result});
+    } catch (error) {
+        res.render("index.ejs", {error: error.message,});
+    }
+    });
 
 
 
